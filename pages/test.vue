7cd847241e0dd98bed8a45eaf6a04a636105b612
ri2112 <riririryou@gmail.com>
2021-02-07 21:29:56 +0900

    testボタン追加

--- pages/test.vue
+++ pages/test.vue
@@ -3,9 +3,27 @@
     <div>
       <Logo />
       <h1 class="title">
-        テスト
+        nuxt-test
       </h1>
-      <v-btn depressed color="error" @click="fetchSomething">ip取得</v-btn>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+ <NuxtLink to="/test">test</NuxtLink>
+ <NuxtLink to="/test2">test2</NuxtLink>
       </div>
     </div>
   </div>
@@ -14,50 +32,51 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({
-  mounted: ()=>{
-    console.log("mounted");
-  },
-  methods: {
-    async fetchSomething() {
-      const ip = await this.$axios.$get('http://icanhazip.com')
-      console.log(ip)
-      return ip
-    }
-  }
-})
+export default Vue.extend({})
 </script>
 
-<style lang="sass">
-// Sample `apply` at-rules with Tailwind CSS
-// .container
-//   @apply min-h-screen flex justify-center items-center text-center mx-auto
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
 
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
 
-.container
-  margin: 0 auto
-  min-height: 100vh
-  display: flex
-  justify-content: center
-  align-items: center
-  text-align: center
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
 
-.title
-  font-family: 'Quicksand','Source Sans Pro',-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,'Helvetica Neue',Arial,sans-serif
-  display: block
-  font-weight: 300
-  font-size: 100px
-  color: #35495e
-  letter-spacing: 1px
-
-.subtitle
-  font-weight: 300
-  font-size: 42px
-  color: #526488
-  word-spacing: 5px
-  padding-bottom: 15px
-
-.links
-  padding-top: 15px
-
+.links {
+  padding-top: 15px;
+}
 </style>
